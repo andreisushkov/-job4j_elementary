50bce85340e6479720046b4d58c4860dda6ee4e3
andrei <andreisushkov@mail.ru>
2022-03-23 12:43:38 +0300

    1.2. Входные данные в тесте исправление1[#297750]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -21,6 +21,7 @@ public class X2Test {
         }
 
         @Test
+
         public void whenA1B1C1X1Then3() {
             int a = 1;
             int b = 1;
@@ -33,6 +34,7 @@ public class X2Test {
         }
 
         @Test
+
         public void whenA0B1C1X1Then3() {
         int a = 0;
         int b = 1;
@@ -44,6 +46,7 @@ public class X2Test {
         }
 
         @Test
+
         public void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
@@ -55,7 +58,8 @@ public class X2Test {
         }
 
         @Test
-    public void whenA1B1C1X0Then1() {
+
+        public void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
         int c = 1;
