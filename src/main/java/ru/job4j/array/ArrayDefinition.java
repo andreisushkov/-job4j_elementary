9d518219aa861c3813147cca980f2922e002054b
andrei <andreisushkov@mail.ru>
2022-04-09 12:01:07 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Андрей Сушков";
+        names[1] = "Владимир Ершов";
+        names[2] = "Антон Жук";
+        names[3] = "Михаил Исахонян";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
